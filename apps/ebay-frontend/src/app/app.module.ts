bcc4472777c43f0a92857b65750eb08408cb77b5
Tshegofatsho Motlatle <tshegomotlatle87@gmail.com>
2023-06-02 21:30:18 +0200

    💄 add sellNFT ui

--- apps/ebay-frontend/src/app/app.module.ts
+++ apps/ebay-frontend/src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FirestoreModule, getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { SellNFTComponent } from './sell-nft/sell-nft.component';
+import { AppRoutingModule } from './app-routing.module';
+import { environment } from '../environments/environment';
+import { provideFirebaseApp } from '@angular/fire/app';
+import { initializeApp } from 'firebase/app';
+
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent, SellNFTComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, SellNFTComponent],
+  imports: [BrowserModule,
+    AppRoutingModule,
+    FirestoreModule,
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
