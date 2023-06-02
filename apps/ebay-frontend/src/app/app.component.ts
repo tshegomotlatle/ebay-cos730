809d70186a2c88ff6cb3ec006d9fbcd19800be91
Tshegofatsho Motlatle <tshegomotlatle87@gmail.com>
2023-06-02 20:30:07 +0200

    🎉 Create a new project

---
+++ apps/ebay-frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ebay-cos730-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'ebay-frontend';
+}
